482ad532cabf4ac736f2ae549a7529b0c0673ce3
Andrew <lvN7yHB*s_I7y2jJ@+ZrgI_a#Ie1L7*%>
2023-06-05 23:40:49 +1000

    second review

--- nearest_zero_1.py
+++ nearest_zero_1.py
@@ -1,4 +1,4 @@
-# id решения  87970669
+# id решения  87970771
 from typing import List
 
 def get_distances_to_nearest_zero(series_of_numbers: List[str],
@@ -8,14 +8,15 @@ def get_distances_to_nearest_zero(series_of_numbers: List[str],
     zeros = [position for position, value in enumerate(series_of_numbers)
              if value == key_value]
     zeros_start = zeros[0]
+    zeros_end = zeros[-1]
     for pointer in range(zeros_start):
         results[pointer] = zeros_start - pointer
     for left, right in zip(zeros, zeros[1:]):
         for pointer in range(left, right):
             if pointer != left:
                 results[pointer] = (min(pointer - left, right - pointer))
-    for pointer in range(zeros[-1], street_length):
-        results[pointer] = pointer - zeros[-1]
+    for pointer in range(zeros_end, street_length):
+        results[pointer] = pointer - zeros_end
     return results
 
 if __name__ == '__main__':
