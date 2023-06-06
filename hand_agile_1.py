c316f1bd253907f78fdc42c4929da0da5f1bd855
Andrew <lvN7yHB*s_I7y2jJ@+ZrgI_a#Ie1L7*%>
2023-06-07 00:07:41 +1000

    sxth review

--- hand_agile_1.py
+++ hand_agile_1.py
@@ -1,15 +1,19 @@
-##ID решения 87991758
+#ID решения 87996137
+from typing import Iterable, Any, Sequence
+
+
 def get_points(
-        values: str,
+        values: Sequence[Any],
         clicks: int,
-        players: int=2,
-        allowed_values: str='123456789'
+        players: int = 2,
+        allowed_values: Iterable[Any] = '123456789'
 ) -> int:
     max_clicks = clicks * players
     return sum(
         0 != values.count(value) <= max_clicks
         for value in allowed_values
     )
+
 
 if __name__ == '__main__':
     print(
--- nearest_zero_1.py
+++ nearest_zero_1.py
@@ -1,23 +1,24 @@
-# id решения  87993386
-from typing import List
+# id решения  87996103
+from typing import List, Sequence, Any
 
-def get_distances_to_nearest_zero(numbers: List[str],
-                                  key_value: str= '0') -> List[int]:
-    street_length = len(numbers)
-    results = [0] * street_length
-    zeros = [pos for pos, value in enumerate(numbers)
-             if value == key_value]
+
+def get_distances_to_nearest_zero(
+        numbers: Sequence[Any],
+        key_value: Any = '0'
+) -> List[int]:
+    length = len(numbers)
+    results = [0] * length
+    zeros = [pos for pos, value in enumerate(numbers) if value == key_value]
     start, end = zeros[0], zeros[-1]
     results[:start] = [start - pos for pos in range(start)]
-    results[start:end] = [
-        min(pos - left, right - pos) if pos != left else 0
-        for left, right in zip(zeros, zeros[1:])
-        for pos in range(left, right)
-    ]
-    results[end + 1:] = [
-        pos - end for pos in range(end + 1, street_length)
-    ]
+    for left, right in zip(zeros, zeros[1:]):
+        results[left + 1:right] = [
+            min(pos - left, right - pos)
+            for pos in range(left + 1, right)
+        ]
+    results[end + 1:] = [pos - end for pos in range(end + 1, length)]
     return results
+
 
 if __name__ == '__main__':
     input()
